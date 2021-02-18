df37a405b6ab55d5c178068400d8766616dd4de4
APiatysheva <a.pyatysheva@yandex.ru>
2021-02-18 12:44:54 +0300

    Added HTML (view) part

--- src/main/java/com/javaAPI/blog_V3/controllers/MainController.java
+++ src/main/java/com/javaAPI/blog_V3/controllers/MainController.java
@@ -1,5 +1,6 @@
 package com.javaAPI.blog_V3.controllers;
 
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +10,15 @@ public class MainController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("title", "The main page"); // ("title" (var) will be transferred into the template;
+        model.addAttribute("title", "Main Page"); // ("title" (var) will be transferred into the template;
                                                             // "The main page" (value of the var) instead of parameter, received in home meth.)
-        return "home"; // open HTML template
+        return "home"; // open HTML template home.html
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "About");
+        return "about";
     }
 
 }
